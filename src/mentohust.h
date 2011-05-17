e99205a5d6f23fca121e34e0b6097680236a1713
Qingping Hou <dave2008713@gmail.com>
2011-05-17 15:15:23 +0800

    add fucntions to load local configuration file. users can use multi accounts which are defined in .mentohust.conf under their home directory. But currently, path for configuration file is written into the source.

---
+++ src/mentohust.h
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  mentohust.h
+ *
+ *    Description:  
+ *
+ *        Created:  05/17/2011 01:11:14 PM
+ *       Revision:  none
+ *
+ *         Author:  Qingping Hou (houqp), dave2008713@gmail.com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef MENTOHUST_H
+#define MENTOHUST_H
+
+#define MENTOHUST_LOG(args...) \
+	printf("mentohust: "args);printf("\n");
+
+#define LOCAL_CONF 1
+
+#define ACCOUNT_SIZE 65
+
+#endif
